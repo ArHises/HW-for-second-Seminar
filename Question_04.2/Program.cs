cc7391ce235f4c9b0880e5e42309e3ee1f6b5160
Arhises <roma1039@gmail.com>
2023-02-04 17:55:02 +0200

    question 4 2d and 3d added

--- Question_04.2/Program.cs
+++ Question_04.2/Program.cs
@@ -1,4 +1,4 @@
-﻿// Найти расстояние между точками в пространстве 2D
+﻿// Найти расстояние между точками в пространстве 3D
 
 Console.WriteLine("Enter X1 number: ");
 int x1 = int.Parse(Console.ReadLine());
@@ -6,15 +6,21 @@ int x1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Enter Y1 number: ");
 int y1 = int.Parse(Console.ReadLine());
 
+Console.WriteLine("Enter Z1 number: ");
+int z1 = int.Parse(Console.ReadLine());
+
 Console.WriteLine("Enter X2 number: ");
 int x2 = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Enter Y2 number: ");
 int y2 = int.Parse(Console.ReadLine());
 
+Console.WriteLine("Enter Z2 number: ");
+int z2 = int.Parse(Console.ReadLine());
+
 int a = x1 - x2;
 int b = y1 - y2;
-double c = System.Math.Sqrt(a*a + b*b);
+int z = z1 - z2;
+double c = System.Math.Sqrt(a*a + b*b + z*z);
 
 Console.WriteLine("The distans between 2 given points is: " + c);
-

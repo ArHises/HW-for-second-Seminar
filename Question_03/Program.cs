a38320bdc31598cf1b296e47f8cf5f622c24840d
Arhises <roma1039@gmail.com>
2023-02-04 16:58:53 +0200

    Third question added

---
+++ Question_03/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine("Enter number between 1 - 4: ");
+int num = int.Parse(Console.ReadLine());
+
+if(num == 1)
+{
+    Console.WriteLine("X = 0 ... + ∞ \nY = 0 ... + ∞");
+}
+else if(num == 2)
+{
+    Console.WriteLine("X = 0 ... - ∞ \nY = 0 ... + ∞");
+}
+else if(num == 3)
+{
+    Console.WriteLine("X = 0 ... - ∞ \nY = 0 ... - ∞");
+}
+else if(num == 4)
+{
+    Console.WriteLine("X = 0 ... + ∞ \nY = 0 ... - ∞");
+}
+else
+{
+    Console.WriteLine("Invalid input");
+}

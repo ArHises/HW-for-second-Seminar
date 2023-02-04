12910cdfac7321114b03112e72d121824be29051
Arhises <roma1039@gmail.com>
2023-02-04 14:32:45 +0200

    First Question added

---
+++ Question_01/Program.cs
@@ -0,0 +1,11 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+Console.WriteLine("Enter number of day: ");
+int day = int.Parse(Console.ReadLine());
+
+if(day == 7){
+    Console.WriteLine("Weekend :)");
+}
+else{
+    Console.WriteLine("go to work :(");
+}

d8c506b216596205ed776b3dde384e9267245263
Arhises <roma1039@gmail.com>
2023-02-04 14:43:33 +0200

    Second question added

---
+++ Question_02/Program.cs
@@ -0,0 +1,16 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+Console.WriteLine("Enter 1st number: ");
+int first = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter 2nd number: ");
+int second = int.Parse(Console.ReadLine());
+
+if( first*first == second){
+
+    Console.WriteLine($"{first}^2 = {second}");
+}
+else{
+    
+    Console.WriteLine("First number is not a square of the second one");
+}
